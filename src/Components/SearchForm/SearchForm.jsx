9503059beb5f0cb6faceaaacd563169651de7e00
KARTIK5799 <thekartiksapkal@gmail.com>
2024-04-08 01:07:40 +0530

    responsive search form done

--- src/Components/SearchForm/SearchForm.jsx
+++ src/Components/SearchForm/SearchForm.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import style from './SearchForm.module.css'
 
 const SearchForm = () => {
   return (
-    <div>
-      
+    <div className={style.searchbarSection}>
+      <form className={style.searchbar}>
+        <input type="text" placeholder='search wanted city' className={style.searchInput}/>
+        <button className={style.searchButton}><span className="material-symbols-outlined">
+search
+</span></button>
+      </form>
     </div>
   )
 }
